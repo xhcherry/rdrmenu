30a98ead2d6da9d322e5f9d6e0266190dccb85e1
Xherry <cyhcherry@outlook.com>
2024-11-22 13:27:04 +0800

    finished renderer

--- src/game/frontend/GUI.cpp
+++ src/game/frontend/GUI.cpp
@@ -1,12 +1,12 @@
 #include "GUI.hpp"
 
-#include "menu/Menu.hpp"
 #include "core/renderer/Renderer.hpp"
+#include "menu/Menu.hpp"
 
 namespace YimMenu
 {
 	GUI::GUI() :
-		m_IsOpen(false)
+	    m_IsOpen(false)
 	{
 		Renderer::AddDXCallback(Menu::Main, -1);
 		Renderer::AddWindowProcedureCallback([this](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
@@ -20,24 +20,26 @@ namespace YimMenu
 
 	void GUI::ToggleMouse()
 	{
-		auto& io = ImGui::GetIO();
-		if (GUI::IsOpen())
-		{
-			io.MouseDrawCursor = true;
-			io.ConfigFlags &= ~ImGuiConfigFlags_NoMouse;
-		}
-		else
-		{
-			io.MouseDrawCursor = false;
-			io.ConfigFlags |= ImGuiConfigFlags_NoMouse;
-		}
+		auto& io           = ImGui::GetIO();
+		io.MouseDrawCursor = GUI::IsOpen();
+		GUI::IsOpen() ? io.ConfigFlags &= ~ImGuiConfigFlags_NoMouse : io.ConfigFlags |= ImGuiConfigFlags_NoMouse;
 	}
 
 	void GUI::WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	{
 		if (msg == WM_KEYUP && wparam == VK_INSERT)
 		{
-			m_IsOpen = !m_IsOpen;
+			//Persist and restore the cursor position between menu instances.
+			static POINT CursorCoords{};
+			if (m_IsOpen)
+			{
+				GetCursorPos(&CursorCoords);
+			}
+			else if (CursorCoords.x + CursorCoords.y)
+			{
+				SetCursorPos(CursorCoords.x, CursorCoords.y);
+			}
+			Toggle();
 			ToggleMouse();
 		}
 	}
